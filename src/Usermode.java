7a9fcd97f42a070c3abb7444074a2f0a206c633e
Home <ryan.sg.shim@gmail.com>
2023-07-17 10:35:52 +0900

    third commit

--- src/Usermode.java
+++ src/Usermode.java
@@ -1,12 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
-public class Usermode {
+public class Usermode extends JFrame {
     ArrayList<Product> cart = new ArrayList<>();
     ArrayList<Product> productList = new ArrayList<>();
     public Usermode() {
-        productListPrint();
-    }
-    public void productListPrint() {
-        DataBaseLoading dataBaseLoading = new DataBaseLoading();
+        super("Kiosk");
+        JLabel label = new JLabel("한예준");
+        label.setSize(10, 20);
+        label.setBounds(100, 200, 10, 20);
+        add(label);
+        setExtendedState(MAXIMIZED_BOTH);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+}
+
+    public static void main(String[] rgs) {
+        new Usermode();
     }
 }
