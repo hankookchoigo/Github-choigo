f03f37c8456f45abea74afee6e42fa48fe82d621
Home <ryan.sg.shim@gmail.com>
2023-07-17 21:19:17 +0900

    Fourth commit

--- src/ModeSelect.java
+++ src/ModeSelect.java
@@ -2,47 +2,92 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class ModeSelect extends JFrame implements ActionListener {
-    /*
-    초기 비밀번호 창을 띄워서 관리자 혹은 사용자로 로그인하도록 하고
-    비밀번호가 그 어느것에도 일치하지 않을 시 다시 입력하게 유도한다.
-     */
-    JPanel p1 = new JPanel();
-    JPanel p2 = new JPanel();
-    // 각각 라벨 패널과 패스워드 패널을 생성
-    
-    JLabel label = new JLabel("아래 칸에 비밀번호를 입력해주세요.");
-    JLabel pwdLabel = new JLabel("password: ");
-    JTextField pwdField = new JTextField(15);
+public class ModeSelect extends JFrame {
     public ModeSelect() {
-        super("순자네 편의점");
-        // 제목 설정
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
         
-        setLayout(new BoxLayout(this, 10));
+        setTitle("Login");
+        setBounds(600, 200, 500, 500);
+        setResizable(true);
         
-        Box labelBox = Box.createHorizontalBox();
-        Box pwdBox = Box.createHorizontalBox();
-        // 박스 설정
+        JLabel label = new JLabel("비밀번호를 입력해 로그인해주세요.");
+        label.setBounds(100, 80, 400, 50);
+        contentPane.add(label);
         
-        labelBox.add(label);
-        labelBox.add(Box.createVerticalStrut(10));
-        // 라벨 박스에 컴포넌트 추가
+        JButton loginButton = new JButton("로그인");
+        loginButton.setBounds(149, 250, 100, 20);
+        contentPane.add(loginButton);
         
-        pwdBox.add(pwdLabel);
-        pwdBox.add(pwdField);
-        //
+        JTextField textField = new JTextField();
+        textField.setBounds(149, 200, 100, 30);
+        contentPane.add(textField);
+        textField.setVisible(true);
         
-        setSize(250, 100);
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(149, 200, 100, 30);
+        contentPane.add(passwordField);
+        passwordField.setVisible(false);
+        
+        String switchToPw = "가리기";
+        String switchToText = "보이기";
+        
+        JButton switchButton = new JButton(switchToPw);
+        switchButton.setBounds(251, 200, 100, 30);
+        contentPane.add(switchButton);
+        
+        String Mpw = "11111";
+        String Upw ="24244";
+        
+        ActionListener loginListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(switchButton.getText().equals(switchToPw)) {
+                    if(textField.getText().equals(Mpw)) {
+                        label.setText("관리자로 로그인합니다.");
+                        Managermode managermode = new Managermode();
+                        dispose();
+                    } else if(textField.getText().equals(Upw)) {
+                        label.setText("사용자로 로그인합니다.");
+                        Usermode usermode = new Usermode();
+                        dispose();
+                    } else {
+                        label.setText("비밀번호가 일치하지 않습니다.");
+                        textField.setText("");
+                    }
+                } else if(switchButton.getText().equals(switchToText)) {
+                    if (passwordField.getText().equals(Mpw)) {
+                        label.setText("관리자로 로그인합니다.");
+                    } else if(passwordField.getText().equals(Upw)) {
+                        label.setText("사용자로 로그인합니다.");
+                    } else {
+                        label.setText("비밀번호가 일치하지 않습니다.");
+                        textField.setText("");
+                    }
+                }
+            }
+        };
+        loginButton.addActionListener(loginListener);
+        
+        ActionListener switchlistener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(switchButton.getText().equals(switchToPw)) {
+                    switchButton.setText(switchToText);
+                    textField.setVisible(false);
+                    passwordField.setVisible(true);
+                    passwordField.setText(textField.getText());
+                 } else if(switchButton.getText().equals(switchToText)) {
+                    switchButton.setText(switchToPw);
+                    textField.setVisible(true);
+                    passwordField.setVisible(false);
+                    textField.setText(passwordField.getText());
+                }
+            }
+        };
+        switchButton.addActionListener(switchlistener);
+        
         setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    
-    @Override
-    public void actionPerformed(ActionEvent e) {
-    
-    }
-    
-    public static void main(String[] args) {
-        ModeSelect modeSelect = new ModeSelect();
     }
 }
